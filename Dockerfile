f154383a8ba54f8811e0e9163043a2892d09c4f9
mu88 <mu88_developer@outlook.com>
2019-12-07 12:24:02 +0100

    Add source code

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+﻿FROM mcr.microsoft.com/windows/servercore:1903
+
+COPY bin/Release/netcoreapp3.0/win-x64/* ./
+
+ENTRYPOINT ["TestWithDocker.exe"]
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestWithDocker
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+
+                DoSomeWork();
+            }
+        }
+
+        private static void DoSomeWork()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                Console.WriteLine(new Random().Next());
+            }
+        }
+    }
+}
